b28593d708c57bdc25c4556ff22d73d3406a6d55
freewind <nowindlee@gmail.com>
2019-05-12 18:03:00 +0800

    improve

--- hello.ts
+++ hello.ts
@@ -2,4 +2,6 @@ import moment from 'moment';
 
 const date = new Date();
 
-console.log(moment(date).format('YYYYMMDD'));
+const format = 'YYYY-MM-DD hh:mm:ss SSS';
+
+console.log(moment(date).format(format));
